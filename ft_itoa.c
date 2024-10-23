9f770d7d2f1b0e142c0582fccd59ac4885645b00
Daryna Shvydka <dshvydka@2-C-2.42heilbronn.de>
2024-10-23 15:54:48 +0200

    Part 2: itoa & strtrim

---
+++ ft_itoa.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dshvydka <dshvydka@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 13:28:56 by dshvydka          #+#    #+#             */
+/*   Updated: 2024/10/23 14:33:50 by dshvydka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	get_num_len(long num)
+{
+	int	len;
+
+	len = 0;
+	if (num == 0)
+		return (1);
+	while (num > 0)
+	{
+		num /= 10;
+		len++;
+	}
+	return (len);
+}
+
+static void	fill_str(char *str, long num, int len)
+{
+	str[len] = '\0';
+	while (len-- > 0)
+	{
+		str[len] = (num % 10) + '0';
+		num /= 10;
+	}
+}
+
+char	*ft_itoa(int n)
+{
+	int		len;
+	int		is_minus;
+	long	num;
+	char	*result;
+	long	temp;
+
+	len = 0;
+	is_minus = 0;
+	num = n;
+	if (n == 0)
+		return (ft_strdup("0"));
+	if (num < 0)
+	{
+		is_minus = 1;
+		num = -num;
+	}
+	temp = num;
+	len = get_num_len(temp) + is_minus;
+	result = (char *)malloc(sizeof(char) * (len + is_minus + 1));
+	if (!result)
+		return (NULL);
+	if (is_minus)
+		result[0] = '-';
+	fill_str(result + is_minus, num, len - is_minus);
+	return (result);
+}
---
+++ ft_strtrim.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dshvydka <dshvydka@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 13:29:45 by dshvydka          #+#    #+#             */
+/*   Updated: 2024/10/23 14:49:19 by dshvydka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*result;
+	int		start;
+	int		end;
+
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	if (!s1 || !set)
+		return (NULL);
+	while (ft_strchr(set, s1[start]) && start <= end)
+		start++;
+	while (ft_strchr(set, s1[end]) && end >= 0)
+		end--;
+	if (start > end)
+		return (ft_strdup(""));
+	result = (char *)malloc(end - start + 2);
+	if (!result)
+		return (NULL);
+	ft_strlcpy(result, &s1[start], end - start + 2);
+	return (result);
+}
